6cb442a037ad7595646cc010cbe1fc727df775ae
SETIANA ANDIKA P <114588737+Setiana467@users.noreply.github.com>
2022-09-28 22:04:18 +0700

    Add files via upload

---
+++ Nodua.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+
+public class Nodua {
+    public static void main(String[] args) {
+        String masukan = JOptionPane.showInputDialog(null, "Anda sedang belajar apa? ");
+
+        JOptionPane.showMessageDialog(null, "Belajar " + masukan + " sangat mudah");
+    }
+}
---
+++ Nosatu.java
@@ -0,0 +1,8 @@
+public class Nosatu {
+    public static void main(String[] args) {
+        System.out.println("Belajar Java");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+
+    }
+}
